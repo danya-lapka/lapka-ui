30fccd8ce5a213aefca7eac8d7e32297582d6aeb
Daniel Vyakulin <danya@danya-lapka.(none)>
2025-10-23 18:57:29 +0400

    config

---
+++ vite.lib.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import dts from 'vite-plugin-dts';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'MyUiKit',
+      fileName: (format) => `my-ui-kit.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+});
