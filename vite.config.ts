c1185bed0bc98138187f851c7e7a6a4c6e90ca62
Daniel Vyakulin <danya@danya-lapka.(none)>
2025-10-26 02:18:46 +0400

    dasdsad

--- vite.config.ts
+++ vite.config.ts
@@ -3,25 +3,17 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import lightningcss from 'vite-plugin-lightningcss'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
-    lightningcss({
-      browserslist: ['>0.5%', 'last 2 versions'],
-    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
-  },
-  build: {
-    minify: 'oxc',
-    cssCodeSplit: true
   },
   base: process.env.NODE_ENV === 'production' ? '/danya-lapka.github.io/lapka-ui/' : '/'
 })
