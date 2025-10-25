27d3f1ddd679567f0bce1c6322250f23a61cfb9a
Daniel Vyakulin <danya@danya-lapka.(none)>
2025-10-26 02:14:51 +0400

    wait

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import '@/styles/all.scss';
 import '@/styles/mixins.scss';
-import { BaseButton, BaseLink, BaseTooltip } from './components';
 
 const app = createApp(App);
-app.component("BaseButton", BaseButton);
-app.component("BaseLink", BaseLink);
-app.component("BaseTooltip", BaseTooltip);
 app.mount('#app');
