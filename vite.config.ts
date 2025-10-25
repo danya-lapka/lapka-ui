a919562ec5c20874af92d15a77aa9965f0f0e57e
Daniel Vyakulin <danya@danya-lapka.(none)>
2025-10-26 01:30:54 +0400

    purgecss

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,41 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import purgeCSS from 'vite-plugin-purgecss'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    purgeCSS({
+      content: [
+        '**/*.html',
+        '**/*.vue',
+        '**/*.ts',
+        '**/*.tsx',
+        '**/*.js',
+        '**/*.jsx',
+      ],
+      safelist: {
+        standard: [
+          /^data-v-/,
+          /^v-/,
+          /^:/,
+          /^::/,
+        ],
+        greedy: [
+          /-(leave|enter|appear)(|-(to|from|active))$/,
+          /^router-link/,
+        ],
+        deep: [
+          /^token/,
+          /^pre/,
+          /^code/,
+        ],
+      },
+      variables: true,
+    }),
   ],
   resolve: {
     alias: {
@@ -18,5 +47,5 @@ export default defineConfig({
   build: {
     minify: true
   },
-   base: process.env.NODE_ENV === 'production'? '/lapka-ui/':'/',
+  base: process.env.NODE_ENV === 'production' ? '/danya-lapka.github.io/lapka-ui/' : '/',
 })
